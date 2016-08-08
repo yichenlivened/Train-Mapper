46ef8751b184ffc7f51bc1f45c32f2d8175d0662
Yi Chen <easoncy435@gmail.com>
2016-08-08 16:15:17 -0400

    Add service-worker

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -5,7 +5,31 @@ angular
     controller: App
   });
 
-function App() {
+function App(dataService) {
   this.todos = [initialTodo];
   this.filter = SHOW_ALL;
+
+  dataService.trainSchedule().then(function(data){
+    console.log(CSVToArray(data.data));
+  });
+
+  function CSVToArray(strData){
+    var arrData = [];
+    var tmp = {};
+    var arrMatches = strData.match(/[^\r\n]+/g);
+    var parametersName = arrMatches.shift().split(',');
+
+    arrMatches.forEach(function(match){
+      tmp = {};
+      match = match.split(',');
+      parametersName.forEach(function(parameter, index){
+        tmp[parameter] = match[index];
+      });
+      arrData.push(tmp);
+    });
+
+    // Return the parsed data.
+    return arrData;
+  }
+
 }
