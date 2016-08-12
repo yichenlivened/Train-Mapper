a4b624902b70d6a3434943c4766d9d912980606c
Yi Chen <easoncy435@gmail.com>
2016-08-12 13:18:15 -0400

    MVC/data update

--- app/scripts/databse.js
+++ app/scripts/databse.js
@@ -1,32 +1,75 @@
-(function(){
-  'use strict';
+'use strict';
 
-  if (!navigator.serviceWorker) return;
-
-  navigator.serviceWorker.register('/sw.js').then(function() {
-    console.log('Registration worked!');
-  }).catch(function() {
-    console.log('Registration failed!');
+//service worker
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js').then(function () {
+    console.log('serviceWorker registered.');
+  }).catch(function (error) {
+    console.log('Error on registering service worker:' + error);
   });
+} else {
+  console.log('Service Worker is not supported in this browser.');
+}
 
-  fetch('/').then(function(response){
-    return response;
-  }).then(function(data){
-    console.log(data);
-  }).catch(function(){
-    console.log('failed');
+//indexedDB
+if (!window.indexedDB) {
+  console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+} else {
+  const DB_NAME = 'train-mapper';
+  const DB_VERSION = 1;
+  var db;
+
+  function openDB() {
+    console.log("openDb ...");
+    var req = indexedDB.open(DB_NAME, DB_VERSION);
+    req.onsuccess = function (evt) {
+      db = this.result;
+      console.log("openDb DONE");
+    };
+    req.onerror = function (evt) {
+      console.error("openDb:", evt.target.errorCode);
+    };
+  }
+}
+
+function txtToArray(txt) {
+  var array = [];
+  var tmp;
+  var arrMatches = txt.match(/[^\r\n]+/g);
+  var parameterNames = arrMatches.shift().split(',');
+  arrMatches.forEach(function (arrMatch) {
+    tmp = {};
+    arrMatch = arrMatch.split(',');
+    arrMatch.forEach(function (data, index) {
+      tmp[parameterNames[index]] = data;
+    });
+    array.push(tmp);
   });
+  return array;
+}
 
-  var GTFSfiles = [
-      'calendar',
-      'calendar_dates',
-  ];
-  fetch('/data/agency.txt').then(function(response){
-    return response.text();
-  }).then(function(data){
-    console.log(data);
-  }).catch(function(){
-    console.log('failed');
+var GTFS;
+
+GTFS = function () {
+  this.filesName = ['calendar', 'calendar_dates', 'stop_times', 'stops', 'trips']
+  this.filesData = {};
+};
+
+GTFS.prototype.filePath = function (fileName) {
+  return '/data/' + fileName + '.txt';
+};
+
+GTFS.prototype.getData = function () {
+  var self = this;
+  self.filesName.forEach(function (fileName) {
+    fetch(self.filePath(fileName)).then(function (res) {
+      return res.text();
+    }).then(function (data) {
+      self.filesData[fileName] = txtToArray(data);
+    }).catch(function (error) {
+      console.log('Error: ', error);
+    });
   });
+  console.log(self.filesData);
+};
 
-})();
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,26 @@
-(function(){
-  'use strict';
+'use strict';
 
-  
-})();
+var model, controller, view;
+
+model = {
+  init: function () {
+    var gtfs = new GTFS;
+    gtfs.getData();
+  }
+};
+
+controller = {
+  init: function () {
+    model.init();
+    view.init();
+  }
+};
+
+view = {
+  init: function () {
+  }
+}
+
+controller.init();
+
 
